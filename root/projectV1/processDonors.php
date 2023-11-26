19cc401087f0e74dfbd34e1d520a66b7d94bf983
B00139870 Komal Wajahat <b00139870@mytudublin.ie>
2023-11-26 22:23:01 +0000

    database is working

--- root/projectV1/processDonors.php
+++ root/projectV1/processDonors.php
@@ -11,7 +11,7 @@ include("dbcon.php");
 $sql = "INSERT INTO donor (firstname, surname, dateofbirth, age, blood_group, contactNumber) 
         VALUES ('$fn', '$sn', '$dob', $age, '$bloodGroup', '$contactNumber')";
 
-//echo $sql; 
+echo $sql; 
 mysqli_query($conn, $sql); 
 mysqli_close($conn); 
 

e3c729a4406ad57523fccfdb2b3e60f14f0a40d7
B00139870 Komal Wajahat <b00139870@mytudublin.ie>
2023-12-07 14:05:58 +0000

    you have been logged in worked

--- processlogin.php
+++ processlogin.php
@@ -18,13 +18,12 @@ while ($row = $result->fetch_assoc()) {
 echo "Database Password: " . $dbpassword . "<br>";
 echo "Given Password: " . $password;
 
-if ($dbpassword == $password) {
-    session_start();
-    $_SESSION['username'] = $username; 
-    header('Location: loggedIn.html'); 
+ if ($pass == $dbpassword) {
+    $_SESSION['username'] = $user;
+    header("Location: loggedIn.html"); 
     echo 'Password matched: Access Granted';
 } else {
-    header('Location: loginTryAgain.html'); 
+    header("Location: loginTryAgain.html"); 
     echo 'Password does not match: Access Denied';
 }
 ?>

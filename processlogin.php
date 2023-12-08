d515c7ad06a2b813a70600a34cbb144895b34372
B00139870 Komal Wajahat <b00139870@mytudublin.ie>
2023-12-08 01:16:50 +0000

    did add session time for 10 mins but for some it wont work

--- processlogin.php
+++ processlogin.php
@@ -1,29 +1,44 @@
 <?php
-$user = $_POST['username'];
-$pass = $_POST['password'];
+session_start();
 
-require("dbcon.php"); 
+// Check for session inactivity
+if (isset($_SESSION['username'])) {
+    $inactive = 60; // Timeout in seconds (10 minutes)
+    $session_life = time() - $_SESSION['login_time_stamp'];
 
-echo "Username: " . $user . "<br>";
-echo "Password: " . $pass . "<br>";
-
-$sql = "SELECT password FROM user WHERE username = '$user'";
-$result = mysqli_query($conn, $sql);
-
-$dbpassword = null; 
-
-while ($row = $result->fetch_assoc()) {
-    $dbpassword = $row['password'];
+    // If the session is inactive for more than the specified duration, destroy the session
+    if ($session_life > $inactive) {
+        session_unset();
+        session_destroy();
+        header("Location: login.html");
+        exit(); // Always include an exit() after a header redirect
+    }
 }
-echo "Database Password: " . $dbpassword . "<br>";
-echo "Given Password: " . $password;
 
- if ($pass == $dbpassword) {
-    $_SESSION['username'] = $user;
-    header("Location: loggedIn.html"); 
-    echo 'Password matched: Access Granted';
-} else {
-    header("Location: loginTryAgain.html"); 
-    echo 'Password does not match: Access Denied';
+// Process login
+if (isset($_POST['username']) && isset($_POST['password'])) {
+    $user = $_POST['username'];
+    $pass = $_POST['password'];
+
+    require("dbcon.php");
+
+    $sql = "SELECT password FROM user WHERE username = '$user'";
+    $result = mysqli_query($conn, $sql);
+
+    $dbpassword = null;
+
+    while ($row = $result->fetch_assoc()) {
+        $dbpassword = $row['password'];
+    }
+
+    if ($pass == $dbpassword) {
+        $_SESSION['username'] = $user;
+        $_SESSION['login_time_stamp'] = time(); // Update login timestamp on successful login
+        header("Location: loggedIn.html");
+        exit(); // Always include an exit() after a header redirect
+    } else {
+        header("Location: loginTryAgain.html");
+        exit(); // Always include an exit() after a header redirect
+    }
 }
 ?>

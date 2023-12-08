3447277f8fdbd5dfe623e8d7e46847d922caf1da
B00139870 Komal Wajahat <b00139870@mytudublin.ie>
2023-12-08 17:58:04 +0000

    session done

--- session.php
+++ session.php
@@ -1,23 +1,10 @@
 <?php
 session_start();
-
-
-$inactive = 600; // 10 minute
-
-
-if (isset($_SESSION['username']) && isset($_SESSION['last_activity'])) {
-    // Calculate the time since the user was last active
-    $session_life = time() - $_SESSION['last_activity'];
-
-    // If the user has been inactive for too long, destroy the session
-    if ($session_life > $inactive) {
-        session_unset();
-        session_destroy();
-        header("Location: loginTryAgain.html"); // Redirect to logout or login page
-        exit();
-    }
+if ($_SESSION['lastAccessed'] < (time() - 600)) {
+    session_destroy();
+    echo "Your session has expired. Please log in again";
+} else {
+    $_SESSION['lastAccessed'] = time();
 }
 
-// Update the last activity time stamp
-$_SESSION['last_activity'] = time();
 ?>

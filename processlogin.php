a0a6186195a301450b42711b9d343813909fd1ae
B00139870 Komal Wajahat <b00139870@mytudublin.ie>
2023-12-08 01:44:59 +0000

    fixed the small errors and the project works

--- processlogin.php
+++ processlogin.php
@@ -3,42 +3,43 @@ session_start();
 
 // Check for session inactivity
 if (isset($_SESSION['username'])) {
-    $inactive = 60; // Timeout in seconds (10 minutes)
+    $inactive = 60; // Timeout in 10 mins
     $session_life = time() - $_SESSION['login_time_stamp'];
 
     // If the session is inactive for more than the specified duration, destroy the session
     if ($session_life > $inactive) {
         session_unset();
         session_destroy();
-        header("Location: login.html");
+        header("Location: LoginForm.html");
         exit(); // Always include an exit() after a header redirect
+        
     }
 }
+$user = $_POST['username'];
+$pass = $_POST['password'];
 
-// Process login
-if (isset($_POST['username']) && isset($_POST['password'])) {
-    $user = $_POST['username'];
-    $pass = $_POST['password'];
+require("dbcon.php"); 
 
-    require("dbcon.php");
+echo "Username: " . $user . "<br>";
+echo "Password: " . $pass . "<br>";
 
-    $sql = "SELECT password FROM user WHERE username = '$user'";
-    $result = mysqli_query($conn, $sql);
+$sql = "SELECT password FROM user WHERE username = '$user'";
+$result = mysqli_query($conn, $sql);
 
-    $dbpassword = null;
+$dbpassword = null; 
 
-    while ($row = $result->fetch_assoc()) {
-        $dbpassword = $row['password'];
-    }
+while ($row = $result->fetch_assoc()) {
+    $dbpassword = $row['password'];
+}
+echo "Database Password: " . $dbpassword . "<br>";
+echo "Given Password: " . $password;
 
-    if ($pass == $dbpassword) {
-        $_SESSION['username'] = $user;
-        $_SESSION['login_time_stamp'] = time(); // Update login timestamp on successful login
-        header("Location: loggedIn.html");
-        exit(); // Always include an exit() after a header redirect
-    } else {
-        header("Location: loginTryAgain.html");
-        exit(); // Always include an exit() after a header redirect
-    }
+ if ($pass == $dbpassword) {
+    $_SESSION['username'] = $user;
+    header("Location: loggedIn.html"); 
+    echo 'Password matched: Access Granted';
+} else {
+    header("Location: loginTryAgain.html"); 
+    echo 'Password does not match: Access Denied';
 }
 ?>
